390348650cda0bf7e7a102fd72d20aba418cf562
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-06-06 20:30:15 +0300

    Added github logo

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="app" class="homeMain">
-    <div class="container">
+  <div id="app">
+    <a href="https://github.com/ilteriskeskin/write-docs" target="_blank">
+      <img src="../assets/images/github.svg" alt="Github Logo">
+    </a>
+
+    <div class="container homeMain">
       <div v-for="doc in docs">
         <ul>
           <li>
@@ -13,35 +17,48 @@
 </template>
 
 <script>
-export default {
-  name: "Home",
+  export default {
+    name: "Home",
 
-  data() {
-    return {
-      docs: {}
-    };
-  },
+    data() {
+      return {
+        docs: {}
+      };
+    },
 
-  created() {
-    this.getDocs();
-  },
+    created() {
+      this.getDocs();
+    },
 
-  methods: {
-    getDocs() {
-      this.$http
-        .get("/public_docs")
-        .then(response => {
-          console.log(response);
-          this.docs = response.data.docs;
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+    methods: {
+      getDocs() {
+        this.$http
+          .get("/public_docs")
+          .then(response => {
+            console.log(response);
+            this.docs = response.data.docs;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
-@import "../assets/css/style.css";
+  @import "../assets/css/style.css";
+
+  img {
+    height: 70px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    cursor: pointer;
+  }
+
+  img:hover {
+    height: 90px;
+  }
 </style>
